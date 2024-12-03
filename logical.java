e35a47da3fc77c02dacef3e6196505fa73e02f05
jeev86399 <nnani86399@gmail.com>
2024-12-03 13:44:34 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class logical{
+    public static void main(String ars[])
+    {
+        System.out.println(true&&true);
+        System.out.println(false||true);
+        System.out.println(!(true));
+    }
+}
